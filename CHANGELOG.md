d39b0126216c1184bea82968218ac9d9011a2a36
Fakhertdinov Marat <marat.fakhertdinov@gmail.com>
2016-09-09 07:46:01 +0700

    Added IpGeoBase

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+CHANGELOG
+=========
+
+0.1.0
+-----
+
+Added functionality:
+
+ * Added IpGeoBase
---
+++ src/IpGeoBase.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Butterfly\Component\IpGeobase;
+
+use IPGeoBase as IPGeoBaseLibrary;
+
+/**
+ * @author Marat Fakhertdinov <marat.fakhertdinov@gmail.com>
+ */
+class IpGeoBase
+{
+    /**
+     * @var IPGeoBaseLibrary
+     */
+    protected $ipGeobaseLibrary;
+
+    /**
+     * @param IPGeoBaseLibrary $ipGeobase
+     */
+    public function __construct(IPGeoBaseLibrary $ipGeobase)
+    {
+        $this->ipGeobaseLibrary = $ipGeobase;
+    }
+
+    /**
+     * @param string $ip
+     * @return string|null
+     */
+    public function getInfo($ip)
+    {
+        $record = $this->ipGeobaseLibrary->getRecord($ip);
+
+        if (false === $record) {
+            return new IpGeoInfo(true);
+        }
+
+        return new IpGeoInfo(
+            false,
+            $ip,
+            $record['range'],
+            $record['cc'],
+            $record['district'],
+            $record['region'],
+            $record['city'],
+            $record['lat'],
+            $record['lng']
+        );
+    }
+}
